cb0487594418c9a35b33eb0e5fb4fa6ae6efd2da
datavers <andras.rozsa@protonmail.com>
2024-08-03 22:06:57 +0200

    Moving to Google Colab

--- portfolio_idea_2024.py
+++ portfolio_idea_2024.py
@@ -2,7 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import requests
+import certifi
+
+response = requests.get('https://yahoo.com', verify=certifi.where())
+print(response.content)
+
 import yfinance as yf 
 
-tickers = yf.Tickers('xle xlb xli')
-print(tickers)
+tickers = yf.Tickers('MSFT XLB XLI')
+aapl = yf.Ticker('AAPL')
+print(f"Ez egy szöveg")
+print(tickers.tickers['MSFT'].history(period="1mo"))
+print(aapl.info)
